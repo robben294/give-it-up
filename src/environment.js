c5aaeb1a7337552d3c55b3259c1e822401feb946
Binjie <luobinjie2017@gmail.com>
2019-04-11 16:17:48 -0400

    Initial commit, set up the moving background

---
+++ src/environment.js
@@ -0,0 +1,27 @@
+
+class Environment {
+    constructor(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.bgPos = 0;
+        this.fgPos = 0;
+        this.bgSpeed = 10;
+        this.bgWidth = 1000;
+        this.bgImg = document.getElementById('bgImg');
+    }
+
+    update() {
+        this.bgPos -= this.bgSpeed;
+        if (this.bgPos < -this.bgWidth) {
+            this.bgPos = 0;
+        }
+    }
+
+    render() {
+        for (let i = 0; i <= this.canvas.width/this.bgWidth + 1; i++) {
+            this.ctx.drawImage(this.bgImg, this.bgPos + i*this.bgWidth, 0);
+        }
+    }
+}
+
+export default Environment;
