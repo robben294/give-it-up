a34c80d11ec07f648873dc9d48a46330fcb22052
Binjie <luobinjie2017@gmail.com>
2019-04-12 10:53:33 -0400

    Add Hero animation

--- src/hero.js
+++ src/hero.js
@@ -5,13 +5,14 @@ class Hero {
         this.ctx = ctx;
         this.width = 40;
         this.height = 40;
-        this.jumping = false;
         this.velocity_Y = 0;
         this.velocity_X = 0;
         // this.sprites = [document.getElementById("turtle1"),
         //     document.getElementById("turtle2"),
         //     document.getElementById("turtle3")
         // ];
+        this.ticks = 0;
+        this.spriteIndex = 0;
         this.sprites = [document.getElementById("hero1"),
                 document.getElementById("hero2")];
 
@@ -41,13 +42,16 @@ class Hero {
     update() { 
         // console.log(this.y); 
         debugger
+        this.ticks++;
+        if (this.ticks % 15.5 === 0) {
+            this.spriteIndex = (this.spriteIndex + 1) % this.sprites.length;
+        }
+
         this.x += this.velocity_X;
         this.y += this.velocity_Y;
-        // if (this.y <= 320) {
-            this.velocity_Y += 0.4; // down gravity
-        // }
+        this.velocity_Y += 0.4; // down gravity
 
-        if (this.y >= 350) {
+        if (this.y >= 350) { //bounce
             this.velocity_Y = -6;
         }
     }
@@ -55,7 +59,7 @@ class Hero {
     render() {
         let renderX = this.x - this.width / 2;
         let renderY = this.y - this.height / 2;
-        this.ctx.drawImage(this.sprites[1], renderX, renderY);
+        this.ctx.drawImage(this.sprites[this.spriteIndex], renderX, renderY);
     }
 }
 
